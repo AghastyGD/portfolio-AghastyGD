a2cf0d200ea82d5b8feea0c234c2a229dbaeb163
Aghasty GD <95558305+AghastyGD@users.noreply.github.com>
2023-01-08 17:37:46 +0200

    formatt

--- script.js
+++ script.js
@@ -30,5 +30,7 @@ $(document).ready(function(){
     $('.menu-btn').click(function(){
         $('.navbar .menu').toggleClass("active");
         $('.menu-btn i').toggleClass("active");
+       return false;
     });
-});
+    
+});z
